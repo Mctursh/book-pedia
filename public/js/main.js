4fb6698e03c77095004ce164370008710fd1d914
mctursh <adenugaayoadehenry63@gmail.com>
2021-09-27 00:29:01 +0100

    search bar functionality complete

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,18 @@
-let prev;
+let prev, isPresent = false, isFound = false;
+
+
 const _fetch = (str) => {
     if (prev !== str) {
-        fetch(`/books/${str}`).then((res) => {
-            console.log(res);
+        fetch(`/books/${str}`)
+        .then((res) => {
             return res.json()
-        }).then(data => {
-            console.log(data);
+        })
+        .then(({data}) => {
+            if (data.length == 0) {
+                isFound = false // No result found
+            } else if (data.length > 0) {
+                updateSuggestions(data) //updating the suggestion modal
+            }
         })
     }
     prev = str;
@@ -17,12 +24,46 @@ const handleFocus = (id) => {;
             let inp = $(id).val();    
             document.querySelector("#search-book").action = `book/${inp}`  //setting the post url 
             if (inp.length > 0) {
-                _fetch(inp)    
+                $(".suggestions").removeClass("hide")
+                isPresent ? $(".loader_parent, #no_result").addClass("hide") : ($(".loader_parent, lds-roller").removeClass("hide"), $("#no_result").addClass("hide"))
+                !isFound && isPresent && noResult()
+                _fetch(inp)
+            } else {
+                $(".suggestions").addClass("hide")
             }
 
             if (document.activeElement !== document.querySelector(id)) {
-                clearInterval(interval)
+                clearInterval(interval) //ends the interval once the input looses focus
             }
         }, 100)    
     } 
 }
+
+function updateSuggestions(params) {
+    isPresent = true
+    isFound = true
+    $("#no_result").addClass("hide")
+    $(".loader_parent").addClass("hide")
+    $(".suggested_courses").removeClass("hide")
+    const sug = $("#sug")[0]
+    const format = (id) => {
+        return `<li>
+                    <a href='books/${id.nativeName}'>
+                        <p>${id.nativeName}</p>
+                    </a>
+                </li>`
+    }
+    let suggestionList = '';
+    params.map(item => suggestionList += format(item))
+    sug.innerHTML = suggestionList
+}
+
+function noResult() {
+    $("#no_result, .loader_parent").removeClass("hide")
+    $(".suggested_courses, .lds-roller").addClass("hide")
+}
+
+// function resultFound() {
+//     $(".loader_parent, #no_result").addClass("hide")
+//     $(".lds-roller").removeClass("hide")
+// }
